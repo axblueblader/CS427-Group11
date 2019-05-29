96ecdcf75d9f9cb1dd2d5d20a749e1352bdf7b3b
Nguyen Quoc Viet <blueassassinx@gmail.com>
2019-05-29 19:07:16 +0700

    A01 commit

---
+++ A01/Assets/LeaderScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderScript : MonoBehaviour
+{
+    public Vector2 newPos;
+    public GameObject test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPos = new Vector2(-8.155842f, -1.48468f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPos = new Vector2(-7.155842f, -1.48468f);
+        GetComponent<Transform>().position = newPos;
+        Debug.Log(test);
+    }
+}
